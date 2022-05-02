74c976ba7662f9c6352ae34c588dd849526f0aae
Adam Šimek <xsimek1@mendelu.cz>
2022-05-02 18:43:44 +0200

    Vytvoření hlavičkového a implementačního souboru třídy Village.

---
+++ Hra/Village.cpp
@@ -0,0 +1,5 @@
+//
+// Created by adams on 02.05.2022.
+//
+
+#include "Village.h"
---
+++ Hra/Village.h
@@ -0,0 +1,15 @@
+//
+// Created by adams on 02.05.2022.
+//
+
+#ifndef HRA_VILLAGE_H
+#define HRA_VILLAGE_H
+
+
+class Village {
+    
+
+};
+
+
+#endif //HRA_VILLAGE_H
