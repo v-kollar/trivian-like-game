27490a9c338d82794ed19825e055318cf5ef2045
Adam Šimek <xsimek1@mendelu.cz>
2022-05-02 19:00:36 +0200

    Vytvoření metod třídy Village v hlavičkové souboru a implementace konstruktoru, getMinVillagers a getNumOfVillagers.

--- Hra/Village.cpp
+++ Hra/Village.cpp
@@ -3,3 +3,20 @@
 //
 
 #include "Village.h"
+
+Village::Village(int minVillagers, int numOfVillagers, int wood, int stone, int iron, int wheat) {
+    m_minVillagers = minVillagers;
+    m_numOfVillagers = numOfVillagers;
+    m_wood = wood;
+    m_stone = stone;
+    m_iron = iron;
+    m_wheat = wheat;
+}
+
+int Village::getMinVillagers() {
+    return m_minVillagers;
+}
+
+int Village::getNumOfVillagers() {
+    return m_numOfVillagers;
+}
--- Hra/Village.h
+++ Hra/Village.h
@@ -6,8 +6,25 @@
 #define HRA_VILLAGE_H
 
 
+#include <vector>
+#include "Building.h"
+
 class Village {
-    
+    int m_minVillagers; //minimální počet vesničanů na dané kolo
+    int m_numOfVillagers; //aktuální stav vesničanů pro toto kolo
+    std::vector<Building*> m_buildings;
+    int m_wood; //počet jednotek suroviny dřeva
+    int m_stone; //počet jednotek suroviny kamene
+    int m_iron; //počet jednotek suroviny železa
+    int m_wheat; //počet jednotek suroviny obilí
+public:
+    Village(int minVillagers, int numOfVillagers, int wood, int stone, int iron, int wheat);
+    void addBuilding();
+    void upgrade();
+    void setVillagers(int number);
+    int getMinVillagers();
+    int getNumOfVillagers();
+    void addNewResources();
 
 };
 
