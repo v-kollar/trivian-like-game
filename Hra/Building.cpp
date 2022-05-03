3fb0f646494673a908ae486a9dabb40b0acb4b68
Vojtěch Vlček <xvlcek3@mendelu.cz>
2022-05-03 09:37:08 +0200

    Implementace tridy Building

--- Hra/Building.cpp
+++ Hra/Building.cpp
@@ -3,3 +3,26 @@
 //
 
 #include "Building.h"
+
+Building::Building(std::string type, int locationRow, int locationCol, int level) {
+    m_type = type;
+    m_locationRow = locationRow;
+    m_locationCol = locationCol;
+    m_level = level;
+}
+
+std::string Building::getType() {
+    return m_type;
+}
+
+int Building::getLocationRow() {
+    return m_locationRow;
+}
+
+int Building::getLocationCol() {
+    return m_locationCol;
+}
+
+int Building::getLevel() {
+    return m_level;
+}
