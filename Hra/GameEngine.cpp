871732d7b52800f566a3de8a2a54e6ea021028ca
Vojtěch Kollár <xkollar3@mendelu.cz>
2022-05-06 18:21:13 +0200

    Implementace nekterych funkci z Game Engine

--- Hra/GameEngine.cpp
+++ Hra/GameEngine.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
 #include "GameEngine.h"
+
+GameEngine::GameEngine() = default;
+
+void GameEngine::setup() {
+
+}
+
+void GameEngine::setDifficulty(int difficulty) {
+
+}
+
+int GameEngine::getDifficulty() const {
+    return m_difficulty;
+}
+
+void GameEngine::printIntro() {
+    std::cout << "Some epic intro...\n";
+}
+
+void GameEngine::printInfo() {
+    std::cout << "Info about the game...\n";
+}
+
+void GameEngine::printOutro() {
+    std::cout << "Some epic outro about not giving up and keep playing...\n";
+}
+
+void GameEngine::printMap() {
+    // prints a map
+}
+
+void GameEngine::play() {
+    // ?
+}
+
+void GameEngine::endRound() {
+    // indicates, that round has ended
+}
--- Hra/GameEngine.h
+++ Hra/GameEngine.h
@@ -4,20 +4,20 @@
 #include "Village.h"
 
 class GameEngine {
-    int m_difficulty;
-    Village* m_village;
+    int m_difficulty{};
+    Village* m_village{};
 
 public:
-    GameEngine()
-    void setup()
-    void setDifficulty(int difficulty)
-    int getDifficulty()
-    void printIntro()
-    void printInfo()
-    void printOutro()
-    void printMap()  // to be added
-    void play()
-    void endRound()
+    GameEngine();
+    void setup();
+    void setDifficulty(int difficulty);
+    int getDifficulty() const;
+    static void printIntro();
+    static void printInfo();
+    static void printOutro();
+    void printMap();  // to be added
+    void play();
+    void endRound();
 };
 
 #endif //ZOO_PROJECT_GAMEENGINE_H
