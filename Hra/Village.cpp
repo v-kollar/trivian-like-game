4852841e6018b897eff471faf510febd3701da68
Vojtěch Kollár <xkollar3@mendelu.cz>
2022-05-06 23:30:44 +0200

    Drobne upravy metod a textu

--- Hra/Village.cpp
+++ Hra/Village.cpp
@@ -15,11 +15,11 @@ Village::Village(int minVillagers, int numOfVillagers, int wood, int stone, int 
     m_wheat = wheat;
 }
 
-int Village::getMinVillagers() {
+int Village::getMinVillagers() const {
     return m_minVillagers;
 }
 
-int Village::getNumOfVillagers() {
+int Village::getNumOfVillagers() const {
     return m_numOfVillagers;
 }
 
@@ -27,29 +27,31 @@ void Village::checkTile() {
     int row;
     int col;
     int index = 0;
-    bool check = false;
-    std::cout << "Input row number: " << std::endl;
+    bool isChecked = false;
+    std::cout << "Input row number: \n";
     std::cin >> row;
-    std::cout << "Input column number: " << std::endl;
+    std::cout << "Input column number: \n";
     std::cin >> col;
     while (index != m_buildings.size()) {
-        if (m_buildings.at(index)->Building::getLocationRow() == row && m_buildings.at(index)->Building::getLocationRow() == col ){
-            check = true;
+        if (m_buildings.at(index)->Building::getLocationRow() == row
+        and m_buildings.at(index)->Building::getLocationRow() == col){
+            isChecked = true;
             break;
         }
         index += 1;
     }
-    if (check){
+    if (isChecked){
         upgrade(m_buildings.at(index));
-    } else{
+    } else {
         int type;
-        std::array<std::string , 4> types = {"farm", "quarry", "lumberjackhouse","house"};
+        std::array<std::string, 4> types = {"farm", "quarry", "lumberjackhouse","house"};
         std::string typeName = "";
-        std::cout << "Choose building type\n" << std::endl;
+        std::cout << "Choose building type:\n" << std::endl;
         for (int i = 0; i < types.size(); ++i) {
             std::vector<int> temp;
             temp = ResourceTable::getCostAndMaterial(types.at(i));
-            std::cout << types.at(i) << " - requirements: " << temp.at(0) << " wood, " << temp.at(1) << " stone, " << temp.at(2) << " iron" << std::endl;
+            std::cout << types.at(i) << " - requirements: " << temp.at(0) << " wood, " << temp.at(1)
+                                        << " stone, " << temp.at(2) << " iron" << std::endl;
         }
         /*
         std::cout << "Vyberte typ budovy \n1 = farma\n2 = kamenolom\n3 = dum drevorubce \n4 = obytny dum";
@@ -67,12 +69,9 @@ void Village::checkTile() {
         }
         if (type == 4) {
             typeName = "house";
-        } else{
+        } else {
             addBuilding(typeName,row,col);
         }
-
-
-
     }
     /*
     for (int i = 0; i < m_buildings.size(); ++i) {
@@ -81,27 +80,26 @@ void Village::checkTile() {
         }
     }
     */
-
 }
 
-void Village::upgrade(Building *building) {
+void Village::upgrade(Building* building) const {
     if (building->getType() == "farm"){
-        if (m_iron>0 && m_stone>0 && m_wood>0){ //VYŘEŠIT JAK BUDE FUNGOVAT UPGRADE
+        if (m_iron > 0 and m_stone > 0 and m_wood > 0){ //VYŘEŠIT JAK BUDE FUNGOVAT UPGRADE
             building->setLevel();
         }
     }
     if (building->getType() == "quarry"){ //VYŘEŠIT JAK BUDE FUNGOVAT UPGRADE
-        if (m_iron>0 && m_stone>0 && m_wood>0){
+        if (m_iron > 0 and m_stone > 0 and m_wood > 0){
             building->setLevel();
         }
     }
     if (building->getType() == "lumberjackhouse"){ //VYŘEŠIT JAK BUDE FUNGOVAT UPGRADE
-        if (m_iron>0 && m_stone>0 && m_wood>0){
+        if (m_iron > 0 and m_stone > 0 and m_wood > 0){
             building->setLevel();
         }
     }
     if (building->getType() == "house"){ //VYŘEŠIT JAK BUDE FUNGOVAT UPGRADE
-        if (m_iron>0 && m_stone>0 && m_wood>0){
+        if (m_iron > 0 and m_stone > 0 and m_wood > 0){
             building->setLevel();
         }
     }
@@ -110,15 +108,15 @@ void Village::upgrade(Building *building) {
 void Village::addBuilding(std::string type, int locationRow, int locationCol) {
     std::vector<int> temp;
     temp = ResourceTable::getCostAndMaterial(type);
-    if (m_wood>=temp.at(0) && m_stone>=temp.at(1) && m_iron>=temp.at(2)){
+    if (m_wood>=temp.at(0) and m_stone>=temp.at(1) and m_iron >= temp.at(2)){
         m_wood -= temp.at(0);
         m_stone -= temp.at(1);
         m_iron -= temp.at(2);
-        m_buildings.push_back(new Building(type,locationRow,locationCol,1));
-        std::cout << "Building finished" << std::endl;
+        m_buildings.push_back(new Building(type, locationRow, locationCol,1));
+        std::cout << "Building finished\n";
         //VYŘEŠIT NÁVRAT DO MENU
-    } else{
-        std::cout << "You dont have enough resources" << std::endl;
+    } else {
+        std::cerr << "You don't have enough resources!\n";
         //VYŘEŠIT NÁVRAT DO MENU
     }
 
@@ -126,27 +124,24 @@ void Village::addBuilding(std::string type, int locationRow, int locationCol) {
 
 void Village::addNewResources() {
     for (int i = 0; i < m_buildings.size(); ++i) {
-        if (m_buildings.at(i)->getType()=="farm") {
+        if (m_buildings.at(i)->getType() == "farm") {
             m_wheat += ResourceTable::getProducetQty(m_buildings.at(i));
         }
-        if (m_buildings.at(i)->getType()=="quarry") {
+        if (m_buildings.at(i)->getType() == "quarry") {
             m_stone += ResourceTable::getProducetQty(m_buildings.at(i));
         }
-        if (m_buildings.at(i)->getType()=="lumberjackhouse") {
+        if (m_buildings.at(i)->getType() == "lumberjackhouse") {
             m_wood += ResourceTable::getProducetQty(m_buildings.at(i));
         }
-        if (m_buildings.at(i)->getType()=="house") {
+        if (m_buildings.at(i)->getType() == "house") {
             m_numOfVillagers += ResourceTable::getProducetQty(m_buildings.at(i));
         }
     }
 }
 
-void Village::printResources() {
-    std::cout << "Available resources: " <<
-    "Wood: " << m_wood <<
-    "Stone: " << m_stone <<
-    "Iron: " << m_iron <<
-    "Wheat: " << m_wheat << std::endl << std::endl;
+void Village::printResources() const {
+    std::cout << "Available resources: \n"
+    << m_wood << "x wood, " << m_stone << "x stone, " << m_iron << "x iron, " << m_wheat << "x wheat.\n\n";
 }
 
 void Village::feedVillagers() {
